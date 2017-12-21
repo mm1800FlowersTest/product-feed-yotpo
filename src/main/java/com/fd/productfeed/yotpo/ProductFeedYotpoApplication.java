ccb2d10cb8ed46b4286caa1a9d6e66e47d1df935
mmosttler <mmosttler@harryanddavid.com>
2017-12-20 16:42:29 -0800

    add initial Spring Boot project to test at circleci

---
+++ src/main/java/com/fd/productfeed/yotpo/ProductFeedYotpoApplication.java
@@ -0,0 +1,12 @@
+package com.fd.productfeed.yotpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFeedYotpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFeedYotpoApplication.class, args);
+	}
+}
