68aee1b10da2291a97d74f95f22cb29dec95c284
mmosttler <mmosttler@harryanddavid.com>
2017-12-22 18:10:29 -0800

    test updating the pod and also wanting to test a simple get api

--- src/main/java/com/fd/productfeed/controller/RestApiController.java
+++ src/main/java/com/fd/productfeed/controller/RestApiController.java
@@ -1,8 +1,6 @@
 package com.fd.productfeed.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,8 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
 import com.fd.productfeed.request.ProductsFeed;
 import com.fd.productfeed.utils.FDJacksonUtils;
 
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
+	
+	@GetMapping("/ping")
+	public String ping()
+	{
+		return "hello";
+	}
 	
 // -------------------Create mass product-------------------------------------------
 	 
@@ -57,7 +61,11 @@ public class RestApiController {
         //restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         
         try {
-        	ResponseEntity<String> response = restTemplate.exchange("https://api.yotpo.com/apps/YOUR_APP_KEY/products/mass_create", HttpMethod.POST, httpEntity, String.class);	
+        	ResponseEntity<String> response = restTemplate.exchange("https://api.yotpo.com/apps/YOUR_APP_KEY/products/mass_create", HttpMethod.POST, httpEntity, String.class);
+        	if(response == null)
+        	{
+        		
+        	}
         } catch(HttpStatusCodeException e) {
         	// trying to find response body:
         	String errorpayload = e.getResponseBodyAsString();
