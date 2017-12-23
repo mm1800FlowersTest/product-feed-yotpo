34858a9b8fba56c5737bc1a0e2181437729abc8c
mmosttler <mmosttler@harryanddavid.com>
2017-12-22 19:47:58 -0800

    add another test

--- src/main/java/com/fd/productfeed/yotpo/ProductFeedYotpoController.java
+++ src/main/java/com/fd/productfeed/yotpo/ProductFeedYotpoController.java
@@ -3,6 +3,10 @@ package com.fd.productfeed.yotpo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 /**
  * 
  * @author e019904
@@ -12,9 +16,56 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductFeedYotpoController 
 {
 	@GetMapping("/ping")
-	public String getDeliveryCalendarHello() 
+	public String ping() 
 	{
 		return "Hello from Yotpo Product Feed";		
 	}
 
+	@GetMapping("/sendTestToRabbit")
+	public String sendTestToRabbit() 
+	{
+		String routingKey = "test.topic.all";
+		String message = "Hello World ";
+		String result = "";
+
+		ConnectionFactory factory = new ConnectionFactory();
+		//factory.setHost("localhost");
+		factory.setHost("rabbitmq.product-feed-rabbitmq.svc.cluster.local");
+		Connection connection = null;
+		Channel channel = null;
+		try
+		{
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.exchangeDeclare("testTopic", "topic");
+			result = " [x] Sent '" + routingKey +"':'" + message + "'";
+			System.out.println(result);			
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			result = "ERROR sending " + e.getMessage();
+		} 
+		finally
+		{
+			try
+			{
+				if(channel != null)
+				{
+					channel.close();
+				}
+				if(connection != null)
+				{
+					connection.close();
+				}				
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				result = result + " ALSO: " + e.getMessage();
+			}
+		}
+		
+		return result;		
+	}	
 }
